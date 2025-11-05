2d3eb3fdefc6b451d54c96e804dde6da911a2d52
sures <AS4389@live.mdx.ac.uk>
2025-11-05 15:07:34 +0400

    Update: Adding the function 'verify password'

--- authorize.py
+++ authorize.py
@@ -6,10 +6,10 @@ def hash_password(plain_text_pass):
     hashed_password = bcrypt.hashpw(password_bytes, salt)
     return hashed_password
 
+def verify_password(hashed_password, plain_text_pass):
+    password_bytes = plain_text_pass.encode('utf-8')
+    hashed_password_bytes=hashed_password.encode('utf-8')
+    return bcrypt.checkpw(password_bytes, hashed_password_bytes)
 passw="apple"
 password_hash = hash_password(passw)
 print(f'Password hash: {passw},hash,{password_hash}')
-
-
-
-atul = "Idk"
