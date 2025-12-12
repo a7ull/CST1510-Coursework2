eed8fd46300bbdb5d3b436a5ab32d1fb8e6fc2d0
a7ull <AS4389@live.mdx.ac.uk>
2025-12-12 22:01:06 +0400

    Update db.py

--- Week 9/app/data/db.py
+++ Week 9/app/data/db.py
@@ -1,5 +1,6 @@
 import sqlite3
 DB_NAME = "DATA/intelligence_platform.db"
+#Connecting the database to python
 def connect_db():
     conn = sqlite3.connect(DB_NAME)
     conn.row_factory = sqlite3.Row
