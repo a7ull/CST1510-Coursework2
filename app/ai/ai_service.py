847e8004701b7f126d59d41259c9c879604e3900
a7ull <AS4389@live.mdx.ac.uk>
2025-12-12 16:47:34 +0400

    Update ai_service.py

--- app/ai/ai_service.py
+++ app/ai/ai_service.py
@@ -1,5 +1,9 @@
 from google import genai
-client = genai.Client(api_key="api_key")
+import os
+Gemini_KEY = os.getenv("GEMINI_API_KEY", os.getenv("test", ""))
+client = None
+if Gemini_KEY:
+    client = genai.Client(api_key=Gemini_KEY)
 def ask_ai(prompt):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
